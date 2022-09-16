7779a750f8c2b22e295a0cd2755e618e17e31160
abditaresadabela <oumuamua192017@gmail.com>
2022-09-16 09:25:20 +0530

    This program prints the largest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
+#include "main.h"
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
 
-#endif /* MAIN_H *
+int main(void)
+{
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
+}
