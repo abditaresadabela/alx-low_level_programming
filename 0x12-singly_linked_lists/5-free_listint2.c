bd4ddb594f028fa9d4cada62113953aa2bdcfa0e
abditaresadabela <oumuamua192017@gmail.com>
2022-10-25 08:04:48 +0530

    Free

--- 0x12-singly_linked_lists/5-free_listint2.c
+++ 0x12-singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,32 @@
 #include "lists.h"
+
 /**
- * free_listint2 - Frees a list of nodes, and sets the head to NULL
- * @head: Pointer to the first element of the list
+ * free_listint2 - frees all the node in a list and reset the head to NULL.
+ * @head: pointer of pointer to a list.
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-
-	if (head == NULL || *head == NULL)
+	if (!head)
 		return;
-	while ((*head)->next != NULL)
+	if (*head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		temp = *head;
+		*head = NULL;
+		free_listint(temp);
+		return;
 	}
-	free(*head);
-	*head = NULL;
 }
+/**
+ * free_listint - frees all the node in a list.
+ * @head: pointer to a list.
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
+}
+
