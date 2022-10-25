d62b9ad53e1c886228258c5e2053763d20fab86e
abditaresadabela <oumuamua192017@gmail.com>
2022-10-25 08:18:02 +0530

    Task 9

--- 0x12-singly_linked_lists/9-insert_nodeint.c
+++ 0x12-singly_linked_lists/9-insert_nodeint.c
@@ -1,66 +1,51 @@
 #include "lists.h"
 
-
-static listint_t *new_node(const int n);
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given
- * position.
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
  *
- * @head: pointer of pointer to a listint_t
- * @idx: index of the list where the new node should be added
- * @n: value to set the new node to.
- *
- * Return: pointer to the head.
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *temp, *new;
-
-	if (!(*head) && idx == 0)
-	{
-		*head = new_node(n);
-		return (*head);
-	}
-
-	i = 0;
-	temp = *head;
-	while (temp)
-	{
-		if (idx == 0)
-		{
-			*head = new_node(n);
-			(*head)->next = temp;
-			return (*head);
-		}
-		else if (i == idx - 1)
-		{
-			new = new_node(n);
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		temp = temp->next;
-		i++;
-	}
-	return (NULL);
-}
-
-
-/**
- * new_node - creates a new listint_t node.
- * @n: number to place inside the node.
- *
- * Return: pointer to the new node.
- */
-static listint_t *new_node(const int n)
-{
 	listint_t *new;
+	listint_t *h;
+
+	h = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
+	}
+
+	if (h == NULL && idx != 0)
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+
 	return (new);
 }
